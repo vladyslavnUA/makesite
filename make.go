de0bc2a6068c761a348f0d0319684df30e40451a
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-12 21:31:19 -0400

    generating new files

--- make.go
+++ make.go
@@ -1,29 +1,66 @@
 package main
 
 import (
-	// s"fmt"
+	"os"
+	"fmt"
 	"io/ioutil"
+	"html/template"
+	"flag"
+	"strings"
 )
 
-func main() {
-	ex := readFile("first-post.html")
-	return ex
+// data where filecontents is stored, dictionary
+type FileData struct {
+	Title string
+	Content string
 }
 
-// data where filecontents is stored, dictionary
-type data struct {
-	content string
-}
-func readFile(name string) data {
-	fileContents, err := ioutil.ReadFile(name)
+func main() {
+	var err error
+	// var content string
+	
+	// FLAGS
+	filename := flag.String("file", "", " the name of your .txt file")
+	flag.Parse()
+	fmt.Println("file: ", *filename)
+
+	fileContents, err := ioutil.ReadFile("latest-post.txt")
+	fmt.Println(string(fileContents))
+	////////
+
+	content := FileData {
+		Title: "latest-post",
+		Content: string(fileContents),
+	}
+	t := template.Must(template.ParseFiles("template.tmpl"))
+
+	newfile, err := os.Create("latest-post.html")
+	err = t.Execute(os.Stdout, content)
+	err = t.Execute(newfile, content)
 	if err != nil {
 		panic(err)
 	}
+	// return FileData{Content: string(content)}
+}
+
+
+func readFile(filename string) FileData {
 	var content string
+	fileContents, err := ioutil.ReadFile("latest-post.txt")
+	if err != nil {
+		panic(err)
+	}
 	content = string(fileContents)
-	return data{content: string(content)}
+	return FileData{Content: strings.TrimSpace(string(content))}
 }
 
 func renderTemplate() string{
-	return "hello"
+	var filename string
+	bytesToWrite := []byte(filename)
+	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(err)
+	return fmt.Sprintf("%s file created", err)
 }
