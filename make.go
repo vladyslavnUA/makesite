4621734e23f276fd41eed49eca4c1f4de2020be9
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-12 21:35:41 -0400

    refactoring code to save

--- make.go
+++ make.go
@@ -16,6 +16,33 @@ type FileData struct {
 }
 
 func main() {
+	save()
+	// return FileData{Content: string(content)}
+}
+
+
+func readFile(filename string) FileData {
+	var content string
+	fileContents, err := ioutil.ReadFile("latest-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	content = string(fileContents)
+	return FileData{Content: strings.TrimSpace(string(content))}
+}
+
+func renderTemplate() string{
+	var filename string
+	bytesToWrite := []byte(filename)
+	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(err)
+	return fmt.Sprintf("%s file created", err)
+}
+
+func save() {
 	var err error
 	// var content string
 	
@@ -40,27 +67,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// return FileData{Content: string(content)}
-}
-
-
-func readFile(filename string) FileData {
-	var content string
-	fileContents, err := ioutil.ReadFile("latest-post.txt")
-	if err != nil {
-		panic(err)
-	}
-	content = string(fileContents)
-	return FileData{Content: strings.TrimSpace(string(content))}
-}
-
-func renderTemplate() string{
-	var filename string
-	bytesToWrite := []byte(filename)
-	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(err)
-	return fmt.Sprintf("%s file created", err)
 }
